97fcec48f67af7c0266b5201902ddb169c704743
Michael90Carter <mikeamone@gmail.com>
2024-09-08 12:24:58 +0400

    Add views

---
+++ Amara_Restaurant/wwwroot/pages/enumerators.js
@@ -0,0 +1,28 @@
+﻿"use strict";
+// Class definition
+
+$(document).ready(function () {
+
+
+    // show modal
+    function showModal() {
+        $('#exampleModal').modal('show');
+    }
+
+    //Hide modal
+
+    function hideModal() {
+        $('#exampleModal').modal('hide');
+    }
+
+    //Button When clicked event
+    $('#showModalButton').click(function () {
+        showModal();
+    })
+
+
+    $('#exampleModal').on('hidden.bs.modal', function () {
+        console.log ('Modal hidden')
+    })
+
+})
